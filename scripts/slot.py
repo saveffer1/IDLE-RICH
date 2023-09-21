07ca7a3b03570d73eb8721cae741784acf0f834f
saveffer1 <saveffer01@gmail.com>
2023-09-21 17:28:38 +0700

    add check winline function

--- scripts/slot.py
+++ scripts/slot.py
@@ -1,325 +1,197 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
 import random
-from settings import slot_img
 
 class SlotItem:
-    def __init__(self, name:str, image:any, emoji:str="", 
-                 stack:set={3:15, 4:45, 5:200}, skill:str="price"):
-        self.name = name
-        self.image = image
-        self.emoji = emoji
-        self.stack = stack
-        if skill not in ["price", "spin", "wild", "bonus"]:
-            self.skill = "price"
+    next_id = 0
+    def __init__(self, symbol: str, image: pygame.Surface, probability, skill: str="price", payout: dict={3:25, 4:50, 5:200}):
+        self.id = SlotItem.next_id
+        SlotItem.next_id += 1
+        self.symbol = symbol
+        self.probability = probability
+        self.payout = payout
+        if skill not in ["price", "wild", "scatter", "bonus"]:
+            raise Exception("Invalid skill type. Can only be 'price', 'wild', or 'scatter'")
         else:
             self.skill = skill
+
+class SlotSystem:
+    def __init__(self, min_bet, paylines: list=[]):
+        self.min_bet = min_bet
+        self.max_bet = 15 * min_bet
+        
+        #0
+        self.banana = SlotItem("🍌", pygame.image.load('assets/slot/item/banana.png'), 20, "price", {3:25, 4:50, 5:200})
+        #1
+        self.grape = SlotItem("🍇", pygame.image.load('assets/slot/item/grape.png'), 20, "price", {3:25, 4:50, 5:200})
+        #2
+        self.cherry = SlotItem("🍒", pygame.image.load('assets/slot/item/cherry.png'), 15, "price", {3:15, 4:45, 5:150})
+        #3
+        self.lemon = SlotItem("🍋", pygame.image.load('assets/slot/item/lemon.png'), 15, "price", {3:15, 4:45, 5:150})
+        #4
+        self.bigprize = SlotItem("💰", pygame.image.load('assets/slot/item/bigprize.png'), 5, "price", {3:150, 4:250, 5:500})
+        #5
+        self.scatter = SlotItem("🎲", pygame.image.load('assets/slot/item/freespin.png'), 10, "scatter", {3:25, 4:50, 5:200})
+        #6
+        self.bonusgame = SlotItem("🃏", pygame.image.load('assets/slot/item/game.png'), 5, "bonus", {3:150, 4:250, 5:500})
+        #7
+        self.jackpot = SlotItem("👑", pygame.image.load('assets/slot/item/jackpot.png'), 2, "price", {3:250, 4:500, 5:2000})
+        #8
+        self.wild = SlotItem("🍀", pygame.image.load('assets/slot/item/wildcard.png'), 8, "wild", {3:15, 4:45, 5:150})
+        
+        self.normal_reel = [
+            self.banana, self.grape, self.cherry, 
+            self.lemon, self.bigprize, self.jackpot, 
+            self.bonusgame, self.scatter, self.wild
+        ]
+        
+        self.no_wild_reel = [
+            self.banana, self.grape, self.cherry, 
+            self.lemon, self.bigprize, self.jackpot, 
+            self.bonusgame, self.scatter
+        ]
+
+        self.paylines = [
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [2, 2, 2, 2, 2],
             
-        self.next = None
-        self.prev = None
-
-class SlotMachine:
-    def __init__(self, rows=3, columns=5):
-        self.head = None
-        self.tail = None
-        self.length = 0
-        self.rows = rows
-        self.nTilesPerCol = rows
-        self.columns = columns
-        self.reels = []    
-        self.spin_count = 0
-        
-        self.winlines = [
-            (# winline 1
-                [0, 0, 0, 0, 0],
-                [0, 0, 1, 0, 0],
-                [1, 1, 0, 1, 1]
-            ),
-            (# winline 2
-                [1, 1, 0, 1, 1],
-                [0, 0, 1, 0, 0],
-                [0, 0, 0, 0, 0]
-            ),
-            (# winline 3
-                [0, 0, 0, 0, 0],
-                [1, 0, 0, 0, 1],
-                [0, 1, 1, 1, 0]
-            ),
-            (# winline 4
-                [0, 1, 1, 1, 0],
-                [1, 0, 0, 0, 1],
-                [0, 0, 0, 0, 0]
-            ),
-            (# winline 5
-                [1, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
-            ),
-            (# winline 6
-                [0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0]
-            ),
-            (# winline 7
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 1]
-            ),
-            (# winline 8
-                [0, 0, 1, 0, 0],
-                [0, 1, 0, 1, 0],
-                [1, 0, 0, 0, 1]
-            ),
-            (# winline 9
-                [1, 0, 0, 0, 1],
-                [0, 1, 0, 1, 0],
-                [0, 0, 1, 0, 0]
-            ) 
+            [2, 2, 1, 2, 2],
+            [0, 0, 1, 0, 0],
+            
+            [1, 2, 2, 2, 1],
+            [1, 0, 0, 0, 1],
+            
+            [2, 1, 0, 1, 2],
+            [0, 1, 2, 1, 0]
         ]
-        
-        # self.win_patterns = [
-        #     [(2, 1), (2, 2), (1, 3), (2, 4), (2, 5)],
-        #     [(0, 0), (0, 1), (1, 2), (0, 3), (0, 4)],
-        #     [(1, 0), (2, 1), (2, 2), (2, 3), (1, 4)],
-        #     [(1, 0), (0, 1), (0, 2), (0, 3), (1, 4)],
-        #     [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)],
-        #     [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)],
-        #     [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)],
-        #     [(2, 0), (1, 1), (0, 2), (1, 3), (2, 4)],
-        #     [(0, 0), (1, 1), (2, 2), (1, 3), (0, 4)]
-        # ]
-
-        self.win_patterns = [
-            # winline 1
-            [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)],
-            # winline 2
-            [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)],
-            # winline 3
-            [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)],
-            # winline 4
-            [(2, 0), (2, 1), (1, 2), (2, 3), (2, 4)],
-            # winline 5
-            [(0, 0), (0, 1), (1, 2), (0, 3), (0, 4)],
-            # winline 6
-            [(1, 0), (2, 1), (2, 2), (2, 3), (1, 4)],
-            # winline 7
-            [(1, 0), (0, 1), (0, 2), (0, 3), (1, 4)],
-            # winline 8
-            [(2, 0), (1, 1), (0, 2), (1, 3), (2, 4)],
-            # winline 9
-            [(0, 0), (1, 1), (2, 2), (1, 3), (0, 4)]
+    
+        self.reset_reel()
+        self.reels = [
+            self.first_reel, 
+            self.second_reel, 
+            self.third_reel, 
+            self.fourth_reel, 
+            self.fifth_reel
         ]
+
+    def reset_reel(self):
+        self.first_reel = self.no_wild_reel.copy()
+        self.second_reel = self.normal_reel.copy()
+        self.third_reel = self.normal_reel.copy()
+        self.fourth_reel = self.normal_reel.copy()
+        self.fifth_reel = self.no_wild_reel.copy()
+    
+    def calculate_rtp(self, wins, reels):
+        #FIXME: calculate rtp
+        total_payout = 0
+        winline = wins[0]
+        for win in wins:
+            symbol_id = reels[win[0][0]][win[0][1]]
+            if symbol_id in SlotItem.payout:
+                total_payout += SlotItem.payout[symbol_id]
+
+        total_bet = len(self.paylines)
+        rtp = (total_payout / total_bet) * 100
+
+        return rtp
+    
+    def check_for_wins(self, reels):
+        wins = []
+        wild_symbol_id = self.wild.id
+        specialobj = {
+            "wild": [],
+            "bonus": [],
+            "scatter": []
+        }
         
-    def add_item(self, item: SlotItem):
-        if self.tail is None:
-            self.head = item
-            self.tail = item
-            self.head.next = self.tail
-            self.head.prev = self.tail
-            self.tail.next = self.head
-            self.tail.prev = self.head
-        else:
-            item.prev = self.tail
-            item.next = self.head
-            self.tail.next = item
-            self.head.prev = item
-            self.tail = item
+        for reel in range(5):
+            itemrow0 = reels[0][reel]
+            itemrow1 = reels[1][reel]
+            itemrow2 = reels[2][reel]
+            
+            if itemrow0.skill not in "price":
+                specialobj[itemrow0.skill].append((0, reel))
+            if itemrow1.skill not in "price":
+                specialobj[itemrow1.skill].append((1, reel))
+            if itemrow2.skill not in "price":
+                specialobj[itemrow2.skill].append((2, reel))
+
+        for index, winline in enumerate(self.paylines):
+            set_of_symbols = set()
+            duplicate_between = dict()
+            for col, row in enumerate(winline):
+                current = reels[row][col]
+                prev = reels[winline[col - 1]][col - 1] if col != 0 else None
                 
-        self.reels.append(item)
-        
-        self.length += 1
-    
-    def __len__(self):
-        return self.length
-    
-    def shuffle_nodes(self):
-        if self.length > 1:
-            node_list = []
-            current = self.head
-            for _ in range(self.length):
-                node_list.append(current)
-                current = current.next
-
-            # Shuffle the nodes using random.shuffle for better randomness
-            random.seed(None)
-            random.shuffle(node_list)
+                if current.id not in set_of_symbols and current.id != wild_symbol_id:
+                    set_of_symbols.add(current.id)
+                    duplicate_between[current.id] = 1
+                elif col != 0 and prev.id != None:
+                    if current.id in set_of_symbols:
+                        if current.id == prev.id:
+                            duplicate_between[current.id] += 1
+                        if prev.id == wild_symbol_id:
+                            duplicate_between[current.id] += 1
+                    elif current.id == wild_symbol_id:
+                        if prev.id != wild_symbol_id:
+                            duplicate_between[prev.id] += 1
+                        else:
+                            if reels[winline[col - 2]][col - 2].id != wild_symbol_id:
+                                duplicate_between[reels[winline[col - 2]][col - 2].id] += 1
+                            else:
+                                duplicate_between[reels[winline[col - 3]][col - 3].id] += 1
+                            
+            if len(set_of_symbols) <= 1:
+                wins.append(winline)
+            else:
+                for k, v in duplicate_between.items():
+                    if v >= 3:
+                        wins.append(winline)
+                        break
+            print("winline", index+1, duplicate_between)
             
-            # Reconstruct the linked list with shuffled nodes
-            self.head = node_list[0]
-            self.tail = node_list[-1]
-
-            for i in range(self.length):
-                node_list[i].next = node_list[(i + 1) % self.length]
-                node_list[i].prev = node_list[(i - 1) % self.length]
-    
-    def __random(self):
-        if self.head is None:
-            return []
-
-        result = []
-        current = self.head
-        for _ in range(self.rows):
-            temp = []
-            direction = random.choice(["next", "prev"])
-            for __ in range(self.columns):
-                temp.append(current)
-                if direction == "next":
-                    current = current.next
-                elif direction == "prev":
-                    current = current.prev
-            result.append(temp)
-        
-        self.spin_count += 1
-        if self.spin_count >= 5:
-            self.shuffle_nodes()
-            self.spin_count = 0
-
-        return result
-    
-    def check_winlines(self, result):
-        max_prize = 0
-        freespins = 0
-        bonus_flag = False
-        winning_lines = []
-
-        for i, pattern in enumerate(self.win_patterns):
-            valid_pattern = True
-            symbols = []
-
-            for row, col in pattern:
-                if 0 <= row < len(result) and 0 <= col < len(result[0]):
-                    symbol = result[row][col]
-                    symbols.append(symbol)
-
-                    if symbol is None:
-                        valid_pattern = False
-
-            if valid_pattern and len(set(symbols)) == 1 and symbols[0] is not None:
-                winning_lines.append(i + 1)
-                symbol = symbols[0]
-                prize = symbol.stack[len(self.win_patterns[0])]
-
-                if symbol.skill == "freespin":
-                    freespins += symbol.stack[len(self.win_patterns[0])]
-                elif symbol.skill == "bonus":
-                    bonus_flag = True
-
-                if prize > max_prize:
-                    max_prize = prize
-
-        return max_prize, freespins, bonus_flag, winning_lines
-    
-    # def check_winlines(self, result):
-    #     max_prize = 0
-    #     freespins = 0
-    #     winning_lines = []
-    #     bonus_flag = False
-
-    #     for i, winline in enumerate(self.winlines):
-    #         line_prize = 0
-    #         line_freespins = 0
-    #         line_bonus_flag = False
-    #         consecutive_symbols = []
-    #         wildcard_count = 0
-
-    #         for j in range(len(result)):
-    #             line = result[j]
-    #             for k in range(len(line)):
-    #                 symbol = result[j][k]
-
-    #                 if symbol.skill == "freespin":
-    #                     line_freespins += symbol.stack[len(line)]
-    #                 elif symbol.skill == "wild":
-    #                     consecutive_symbols.append(symbol)
-    #                     wildcard_count += 1
-    #                 else:
-    #                     consecutive_symbols.append(symbol)
-
-    #                 if symbol.skill == "bonus":
-    #                     line_bonus_flag = True
-
-    #         # Check if there are 5 wildcards in a line and use their stack value
-    #         if wildcard_count == len(winline):
-    #             line_prize += consecutive_symbols[0].stack[len(winline)]
-
-    #         # Calculate prize if any symbol occurs more than 2 times
-    #         symbol_counts = {symbol: consecutive_symbols.count(symbol) for symbol in set(consecutive_symbols)}
-    #         for symbol, count in symbol_counts.items():
-    #             if count > 2:
-    #                 line_prize += symbol.stack[len(winline)] * (count - 2)
-
-    #         # Check if the line has at least one non-wildcard symbol
-    #         if any(symbol.skill != "wild" for symbol in consecutive_symbols) and line_prize > 0:
-    #             if line_prize > max_prize:
-    #                 max_prize = line_prize
-    #                 freespins = line_freespins
-    #                 winning_lines = [i + 1]
-    #                 if line_bonus_flag:
-    #                     bonus_flag = True
-    #             elif line_prize == max_prize:
-    #                 winning_lines.append(i + 1)
-    #                 if line_bonus_flag:
-    #                     bonus_flag = True
-
-    #     return max_prize, freespins, bonus_flag, winning_lines
-
-
-
-    def spin(self):
-        if self.spin_count == 0:
-            self.shuffle_nodes()
-
-        self.result = self.__random()
-        max_prize, freespins, bonus_flag, winning_lines = self.check_winlines(self.result)
-
-        bonus = "Bonusgame!" if bonus_flag else "No Bonus"
-
-        # Define highlight colors for each winning line
-        highlight_colors = ["\033[91m", "\033[92m", "\033[93m", "\033[94m", "\033[95m", "\033[96m", "\033[97m", "\033[98m", "\033[99m"]
-
-        # Initialize a flag to check if any win was found
-        win_found = False
-
-        for line_idx, row in enumerate(self.result):
-            for symbol_idx, item in enumerate(row):
-                if item is not None:
-                    format = item.emoji +" "+ item.name
-                    if line_idx + 1 in winning_lines:
-                        color = highlight_colors[winning_lines.index(line_idx + 1)]
-                        print(f"{color}{format:10}\033[0m | ", end=" ")
-                        win_found = True
-                    else:
-                        print(f"{format:10} | ", end=" ")
+        return wins, specialobj
+                    
+    def print_slot_machine(self, randreels, id_format=False):
+        symlenght = 21 if id_format else 26
+        print("-" * symlenght)
+        for row in range(3):
+            for col in range(5):
+                if id_format:
+                    symbol = randreels[row][col].id
                 else:
-                    print(" " * 10 + " | ", end=" ")  # Empty cell
-            print()
-
-        print()
+                    symbol = randreels[row][col].symbol
+                if [row, col] in self.paylines:
+                    print(f"*{symbol}*", end=" | ")
+                else:
+                    if col == 0:
+                        print("|", end=" ")
+                    print(symbol, end=" | ")
+            print("\n" + "-" * symlenght)
+    
+    def spin(self, bet):
+        if bet < self.min_bet or bet > self.max_bet:
+            raise Exception(f"Invalid bet amount. Bet must be between {self.min_bet} and {self.max_bet}")
         
-        if win_found:
-            return f"Winning lines: {winning_lines}\nPrize: {max_prize}\nFreespins: {freespins}\n{bonus}"
-        else:
-            return f"You have no luck this time!\n{bonus}"
+        randreels = [[], [],[]]
+        for reel in self.reels:
+            col0 = random.choices(reel, weights=[item.probability for item in reel])[0]
+            col1 = random.choices(reel, weights=[item.probability for item in reel])[0]
+            col2 = random.choices(reel, weights=[item.probability for item in reel])[0]
+            
+            randreels[0].append(col0)
+            randreels[1].append(col1)
+            randreels[2].append(col2)
 
+        # self.print_slot_machine(randreels)
+        self.print_slot_machine(randreels, id_format=True)
+        wins = self.check_for_wins(randreels)
+        prize = 0#self.calculate_rtp(wins, randreels)
+        
+        print(wins[0])
 
 if __name__ == "__main__":
-    symbols = [
-        SlotItem("jackpot", emoji="👑", image=slot_img["jackpot"]),
-        SlotItem("game", emoji="🃏", image= slot_img["game"]),
-        SlotItem("bigprize", emoji="💰", image= slot_img["bigprize"]),
-        SlotItem("freespin", emoji="🆓", image= slot_img["freespin"], skill="freespin"),
-        SlotItem("wildcard", emoji="🍀", image= slot_img["wildcard"], skill="wild"),
-        SlotItem("banana", emoji="🍌", image= slot_img["banana"]),
-        SlotItem("banana", emoji="🍌", image= slot_img["banana"]),
-        SlotItem("cherry", emoji="🍒", image= slot_img["cherry"]),
-        SlotItem("cherry", emoji="🍒", image= slot_img["cherry"]),
-        SlotItem("lemon", emoji="🍋", image= slot_img["lemon"]),
-        SlotItem("lemon", emoji="🍋", image= slot_img["lemon"])
-    ]
-    random.shuffle(symbols)
-    slot_machine = SlotMachine()
-    
-    for symbol in symbols:
-        slot_machine.add_item(symbol)
-    
-    print()
-    spin_result = slot_machine.spin()
-    print(spin_result)
+    slot = SlotSystem(100)
+    slot.spin(100)
